17736fd4a39cc462ffeec5248fe6098fc31ac03c
KajalRoyjuli <86142918+KajalRoyjuli@users.noreply.github.com>
2023-10-20 10:00:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 # create streamlit_app.py
 # created the main python file
 import streamlit
-streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Breakfast Menu')
+streamlit.title('My Mom's New Healthy Dinner')
+streamlit.header('Breakfast Favourite')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
